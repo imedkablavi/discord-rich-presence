9c42586230782e0d559d7b697f965725d400d212
imed@CybrexTech <76208805+imedkablavi@users.noreply.github.com>
2026-08-23 12:13:06 +0300

    Add CYBREX FiveM resource manifest

---
+++ integrations/fivem/cybrex_presence/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CYBREX'
+description 'Privacy-minimal local companion for CYBREX Discord Rich Presence'
+version '0.1.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/bridge.js'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
